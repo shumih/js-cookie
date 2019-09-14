717b2acab30bbef79e219d9c24a875c14e4e936a
Anton Shumikhin <shumih.anton@gmail.com>
2019-09-14 21:01:34 +0300

    implement withDoc

--- src/js.cookie.js
+++ src/js.cookie.js
@@ -5,163 +5,184 @@
  * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
  * Released under the MIT license
  */
-;(function (factory) {
-	var registeredInModuleLoader;
-	if (typeof define === 'function' && define.amd) {
-		define(factory);
-		registeredInModuleLoader = true;
-	}
-	if (typeof exports === 'object') {
-		module.exports = factory();
-		registeredInModuleLoader = true;
-	}
-	if (!registeredInModuleLoader) {
-		var OldCookies = window.Cookies;
-		var api = window.Cookies = factory();
-		api.noConflict = function () {
-			window.Cookies = OldCookies;
-			return api;
-		};
-	}
-}(function () {
-	function extend () {
-		var i = 0;
-		var result = {};
-		for (; i < arguments.length; i++) {
-			var attributes = arguments[ i ];
-			for (var key in attributes) {
-				result[key] = attributes[key];
-			}
-		}
-		return result;
-	}
+(function(factory) {
+  var registeredInModuleLoader;
+  if (typeof define === "function" && define.amd) {
+    define(factory);
+    registeredInModuleLoader = true;
+  }
+  if (typeof exports === "object") {
+    module.exports = factory();
+    registeredInModuleLoader = true;
+  }
+  if (!registeredInModuleLoader) {
+    var OldCookies = window.Cookies;
+    var api = (window.Cookies = factory());
+    api.noConflict = function() {
+      window.Cookies = OldCookies;
+      return api;
+    };
+  }
+})(function() {
+  function extend() {
+    var i = 0;
+    var result = {};
+    for (; i < arguments.length; i++) {
+      var attributes = arguments[i];
+      for (var key in attributes) {
+        result[key] = attributes[key];
+      }
+    }
+    return result;
+  }
 
-	function decode (s) {
-		return s.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
-	}
+  function decode(s) {
+    return s.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent);
+  }
 
-	function init (converter, document) {
-		function api() {}
+  function init(converter, doc) {
+    if (!doc) {
+      doc = document;
+    }
 
-		function set (key, value, attributes) {
-			if (typeof document === 'undefined') {
-				return;
-			}
+    function api() {}
 
-			attributes = extend({
-				path: '/'
-			}, api.defaults, attributes);
+    function set(key, value, attributes) {
+      if (typeof doc === "undefined") {
+        return;
+      }
 
-			if (typeof attributes.expires === 'number') {
-				attributes.expires = new Date(new Date() * 1 + attributes.expires * 864e+5);
-			}
+      attributes = extend(
+        {
+          path: "/"
+        },
+        api.defaults,
+        attributes
+      );
 
-			// We're using "expires" because "max-age" is not supported by IE
-			attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
+      if (typeof attributes.expires === "number") {
+        attributes.expires = new Date(
+          new Date() * 1 + attributes.expires * 864e5
+        );
+      }
 
-			try {
-				var result = JSON.stringify(value);
-				if (/^[\{\[]/.test(result)) {
-					value = result;
-				}
-			} catch (e) {}
+      // We're using "expires" because "max-age" is not supported by IE
+      attributes.expires = attributes.expires
+        ? attributes.expires.toUTCString()
+        : "";
 
-			value = converter.write ?
-				converter.write(value, key) :
-				encodeURIComponent(String(value))
-					.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+      try {
+        var result = JSON.stringify(value);
+        if (/^[\{\[]/.test(result)) {
+          value = result;
+        }
+      } catch (e) {}
 
-			key = encodeURIComponent(String(key))
-				.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
-				.replace(/[\(\)]/g, escape);
+      value = converter.write
+        ? converter.write(value, key)
+        : encodeURIComponent(String(value)).replace(
+            /%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,
+            decodeURIComponent
+          );
 
-			var stringifiedAttributes = '';
-			for (var attributeName in attributes) {
-				if (!attributes[attributeName]) {
-					continue;
-				}
-				stringifiedAttributes += '; ' + attributeName;
-				if (attributes[attributeName] === true) {
-					continue;
-				}
+      key = encodeURIComponent(String(key))
+        .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
+        .replace(/[\(\)]/g, escape);
 
-				// Considers RFC 6265 section 5.2:
-				// ...
-				// 3.  If the remaining unparsed-attributes contains a %x3B (";")
-				//     character:
-				// Consume the characters of the unparsed-attributes up to,
-				// not including, the first %x3B (";") character.
-				// ...
-				stringifiedAttributes += '=' + attributes[attributeName].split(';')[0];
-			}
+      var stringifiedAttributes = "";
+      for (var attributeName in attributes) {
+        if (!attributes[attributeName]) {
+          continue;
+        }
+        stringifiedAttributes += "; " + attributeName;
+        if (attributes[attributeName] === true) {
+          continue;
+        }
 
-			return (document.cookie = key + '=' + value + stringifiedAttributes);
-		}
+        // Considers RFC 6265 section 5.2:
+        // ...
+        // 3.  If the remaining unparsed-attributes contains a %x3B (";")
+        //     character:
+        // Consume the characters of the unparsed-attributes up to,
+        // not including, the first %x3B (";") character.
+        // ...
+        stringifiedAttributes += "=" + attributes[attributeName].split(";")[0];
+      }
 
-		function get (key, json) {
-			if (typeof document === 'undefined') {
-				return;
-			}
+      return (doc.cookie = key + "=" + value + stringifiedAttributes);
+    }
 
-			var jar = {};
-			// To prevent the for loop in the first place assign an empty array
-			// in case there are no cookies at all.
-			var cookies = document.cookie ? document.cookie.split('; ') : [];
-			var i = 0;
+    function get(key, json) {
+      if (typeof doc === "undefined") {
+        return;
+      }
 
-			for (; i < cookies.length; i++) {
-				var parts = cookies[i].split('=');
-				var cookie = parts.slice(1).join('=');
+      var jar = {};
+      // To prevent the for loop in the first place assign an empty array
+      // in case there are no cookies at all.
+      var cookies = doc.cookie ? doc.cookie.split("; ") : [];
+      var i = 0;
 
-				if (!json && cookie.charAt(0) === '"') {
-					cookie = cookie.slice(1, -1);
-				}
+      for (; i < cookies.length; i++) {
+        var parts = cookies[i].split("=");
+        var cookie = parts.slice(1).join("=");
 
-				try {
-					var name = decode(parts[0]);
-					cookie = (converter.read || converter)(cookie, name) ||
-						decode(cookie);
+        if (!json && cookie.charAt(0) === '"') {
+          cookie = cookie.slice(1, -1);
+        }
 
-					if (json) {
-						try {
-							cookie = JSON.parse(cookie);
-						} catch (e) {}
-					}
+        try {
+          var name = decode(parts[0]);
+          cookie =
+            (converter.read || converter)(cookie, name) || decode(cookie);
 
-					jar[name] = cookie;
+          if (json) {
+            try {
+              cookie = JSON.parse(cookie);
+            } catch (e) {}
+          }
 
-					if (key === name) {
-						break;
-					}
-				} catch (e) {}
-			}
+          jar[name] = cookie;
 
-			return key ? jar[key] : jar;
-		}
+          if (key === name) {
+            break;
+          }
+        } catch (e) {}
+      }
 
-		api.setString = function (str) {
-			document.cookie = str;
-		}
-		api.set = set;
-		api.get = function (key) {
-			return get(key, false /* read as raw */);
-		};
-		api.getJSON = function (key) {
-			return get(key, true /* read as json */);
-		};
-		api.remove = function (key, attributes) {
-			set(key, '', extend(attributes, {
-				expires: -1
-			}));
-		};
+      return key ? jar[key] : jar;
+    }
 
-		api.defaults = {};
+    api.setString = function(str) {
+      doc.cookie = str;
+    };
+    api.set = set;
+    api.get = function(key) {
+      return get(key, false /* read as raw */);
+    };
+    api.getJSON = function(key) {
+      return get(key, true /* read as json */);
+    };
+    api.remove = function(key, attributes) {
+      set(
+        key,
+        "",
+        extend(attributes, {
+          expires: -1
+        })
+      );
+    };
 
-		api.withConverter = init;
-		api.withGlobal = init
+    api.defaults = {};
 
-		return api;
-	}
+    api.withConverter = init;
+    api.withGlobal = init;
+    api.withDoc = function(doc) {
+      return init(null, doc);
+    };
 
-	return init(function () {}, typeof document === 'undefined' ? { cookie: '' } : document);
-}));
+    return api;
+  }
+
+  return init(function() {});
+});
